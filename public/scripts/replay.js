32838bc9daa7c80a9aa7fabeddc5b818c6e0186e
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-07-30 10:58:18 -0700

    Add underscore.js to replay file, add indenting to file

--- public/scripts/replay.js
+++ public/scripts/replay.js
@@ -1,8 +1,15 @@
-var gameData = {};
-gameData.board = [
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0]
-];
+// var GameTile = Backbone.Model.extend({
+//   initialize: function(value) {
+//     this.set({value: value});
+//   }
+// });
+// var GameBoard = Backbone.Collection.extend({
+//   model: GameTile,
+//   url: 'api/gameBoardData',
+//   parse: function(response) {
+//     console.log(response);
+//   }
+// });
+
+// var gb = new GameBoard();
+// gb.fetch();
