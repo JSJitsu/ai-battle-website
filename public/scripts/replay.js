f3181a8e132e3967873be8fe8b0bf65d2d4b51dd
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-07-30 11:39:46 -0700

    feature(replay.js): Build out more backbone infrastructure for eventually viewing the game board feature(server.js): Modify gameData object for use with backbone

--- public/scripts/replay.js
+++ public/scripts/replay.js
@@ -7,9 +7,29 @@ var GameBoard = Backbone.Collection.extend({
   model: GameTile,
   url: '/api/gameBoardData',
   parse: function(response) {
-    console.log(response);
+    var flatBoard = _.flatten(response.board.tiles);
+    flatBoard = _.map(flatBoard, function(value, key, list) {
+      return new GameTile(value);
+    });
+    return flatBoard;
+  }
+});
+var GameTileView = Backbone.View.extend({
+
+});
+
+var GameBoardView = Backbone.View.extend({
+  initialize: function() {
+    this.collection.fetch();
+    this.render();
+  },
+  render: function() {
+    this.$el.append('Hi Jake and Greg!')
   }
 });
 
-var gb = new GameBoard();
-gb.fetch();
+window.app = {};
+app.gameBoard = new GameBoard();
+app.gameBoardView = new GameBoardView({ collection: app.gameBoard });
+
+$('body').append(app.gameBoardView.$el)
