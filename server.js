fe76933d09a60fc475233b949063d1462dc62e6b
Yahkob <jakobharclerode@gmail.com>
2014-07-30 10:37:21 -0700

    add gamedata to server

--- server.js
+++ server.js
@@ -6,5 +6,23 @@ var port = process.env.port || 8080;
 // serve up files in public folder
 app.use('/', express.static(__dirname + '/public'));
 
+var router = express.Router();
+
+router.get('/gameBoardData', function(req, res){
+	var gameData = {};
+  
+  gameData.board = [
+	  [1,0,0,0,0],
+	  [0,1,0,0,0],
+	  [0,0,1,0,0],
+	  [0,0,0,1,0],
+	  [0,0,0,0,1]
+  ];
+
+	res.json(gameData);
+});
+
+app.use('/api', router);
+
 app.listen(port);
 console.log('Listening on port: ', port);
