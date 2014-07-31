5ecccde4d32dd207e17f4f55cce61ae0c12bfded
Yahkob <jakobharclerode@gmail.com>
2014-07-30 20:31:54 -0700

    feature(public): add keybinding to scrobble turns

--- public/scripts/slider.js
+++ public/scripts/slider.js
@@ -1,6 +1,27 @@
+//query slider
 var slider = document.querySelector('.slider');
-var init = new Powerange(slider, { hideRange: true, min: 1, max: 300, start: 10});
+//intialize new slider
+var init = new Powerange(slider, { hideRange: true, min: 1, max: 300, start: 1});
+//listens for slider changes and renders backbone view accordingly
 slider.onchange = function() {
 	app.gameBoardView.updateTurn(slider.value);
-  console.log(slider.value)
 };
+
+//listens for keydown events to control scrobbling
+$(document).keydown(function(e){
+  if(e.which === 39){
+  	// app.gameBoardView.updateTurn(slider.value);
+    slider.value++;
+    app.gameBoardView.updateTurn(slider.value);
+  }
+  if(e.which === 37){
+    if(slider.value < 1){
+      return;
+    }
+    else{
+  	  slider.value--;
+  	  app.gameBoardView.updateTurn(slider.value);
+    }
+
+  }
+});
