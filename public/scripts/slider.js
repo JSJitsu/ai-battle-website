3741df91afa0ab7250c9b7b90976424b5c7bd2ee
Yahkob <jakobharclerode@gmail.com>
2014-07-30 23:22:23 -0700

    feature(public): add keydown scrobbling for replays

--- public/scripts/slider.js
+++ public/scripts/slider.js
@@ -4,23 +4,36 @@ var slider = document.querySelector('.slider');
 var init = new Powerange(slider, { hideRange: true, min: 1, max: 300, start: 1});
 //listens for slider changes and renders backbone view accordingly
 slider.onchange = function() {
+	  
 	app.gameBoardView.updateTurn(slider.value);
+
 };
 
 //listens for keydown events to control scrobbling
 $(document).keydown(function(e){
-  if(e.which === 39){
-  	// app.gameBoardView.updateTurn(slider.value);
+  if(e.which === 39 && slider.value < 300){
     slider.value++;
     app.gameBoardView.updateTurn(slider.value);
+      //set actual range handle to range property plus 1 on key down
+      parseInt($('.range-handle').css('left',
+		  parseInt($('.range-handle').css('left')) + 1));
+		  //set status bar
+		  parseInt($('.range-quantity').css('width',
+		  parseInt($('.range-quantity').css('width')) + 1));
   }
   if(e.which === 37){
     if(slider.value < 1){
       return;
     }
+    
     else{
+    	//exactly the same as line 18 but decrementing
   	  slider.value--;
   	  app.gameBoardView.updateTurn(slider.value);
+  	  parseInt($('.range-handle').css('left',
+		  parseInt($('.range-handle').css('left')) - 1));
+		  parseInt($('.range-quantity').css('width',
+		  parseInt($('.range-quantity').css('width')) - 1));
     }
 
   }
