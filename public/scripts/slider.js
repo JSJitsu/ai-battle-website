716f8f94a9825bbeb8128a6c314b76d826f8e252
Yahkob <jakobharclerode@gmail.com>
2014-07-31 11:58:19 -0700

    feature(public): Significantly modularize code in slider script and add current turn display

--- public/scripts/slider.js
+++ public/scripts/slider.js
@@ -1,10 +1,10 @@
 //query slider
 var slider = document.querySelector('.slider');
 //intialize new slider
-var init = new Powerange(slider, { hideRange: true, min: 1, max: 300, start: 1});
+var init = new Powerange(slider, { hideRange: false, min: 1, max: 300, start: 1});
 //listens for slider changes and renders backbone view accordingly
 slider.onchange = function() {
-	  
+	$('.turn-content').text(slider.value + "/300");
 	app.gameBoardView.updateTurn(slider.value);
 
 };
@@ -13,28 +13,12 @@ slider.onchange = function() {
 $(document).keydown(function(e){
   if(e.which === 39 && slider.value < 300){
     slider.value++;
-    app.gameBoardView.updateTurn(slider.value);
-      //set actual range handle to range property plus 1 on key down
-      parseInt($('.range-handle').css('left',
-		  parseInt($('.range-handle').css('left')) + 1));
-		  //set status bar
-		  parseInt($('.range-quantity').css('width',
-		  parseInt($('.range-quantity').css('width')) + 1));
   }
-  if(e.which === 37){
-    if(slider.value < 1){
-      return;
-    }
-    
-    else{
-    	//exactly the same as line 18 but decrementing
-  	  slider.value--;
-  	  app.gameBoardView.updateTurn(slider.value);
-  	  parseInt($('.range-handle').css('left',
-		  parseInt($('.range-handle').css('left')) - 1));
-		  parseInt($('.range-quantity').css('width',
-		  parseInt($('.range-quantity').css('width')) - 1)); 
-    }
-
+  if(e.which === 37 && slider.value > 1){
+    slider.value--;
   }
+  $('.turn-content').text(slider.value + "/300");
+  app.gameBoardView.updateTurn(slider.value);
+  $('.range-handle').css('left',parseInt(slider.value));
+	$('.range-quantity').css('width',parseInt(slider.value));
 });
