d8734738e1fa5312f6a47ac67a33cebd1ab3ce2e
Yahkob <jakobharclerode@gmail.com>
2014-08-01 16:56:04 -0700

    feature(public): add icon rendering for game board add test data to board(server.js)

--- public/scripts/BoardTileView.js
+++ public/scripts/BoardTileView.js
@@ -2,10 +2,19 @@ var BoardTileView = Backbone.View.extend({
   tagName: 'td',
   className: 'battle-tile',
   initialize: function(){
+    this.assets = {
+      R: "../resources/rock.jpg",
+      H: "../resources/bknight.jpg",
+      D: "../resources/mine.jpg"
+    } 
   	this.render();
     this.model.on('change', this.render());
   },
   render: function() {
-    this.$el.html(this.model.get('value'));
+    var propertyCode = this.model.get('value').charAt(0);
+    if(this.assets[propertyCode]){
+      var $icon = '<img src="' +this.assets[propertyCode] + '">';
+      this.$el.html($icon);
+    }
   }
 });
