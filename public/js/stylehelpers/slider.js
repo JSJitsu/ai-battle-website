8f448c2e6a4e9af49279314a89ed8abba518fd14
Yahkob <jakobharclerode@gmail.com>
2014-08-04 18:38:20 -0700

    feature(public): add character sprites

--- public/js/stylehelpers/slider.js
+++ public/js/stylehelpers/slider.js
@@ -1,7 +1,7 @@
 //query slider
 var slider = document.querySelector('.slider');
 //intialize new slider
-var init = new Powerange(slider, { hideRange: false, min: 1, max: 300, start: 1});
+var init = new Powerange(slider, { hideRange: false, min: 1, max: 2000, start: 1});
 //listens for slider changes and renders backbone view accordingly
 
 slider.onchange = function() {
@@ -9,13 +9,12 @@ slider.onchange = function() {
 };
 //listens for keydown events to control scrobbling
 $(document).keydown(function(e){
-  if(e.which === 39 && slider.value < 300){
+  if(e.which === 39 && slider.value < 2000){
     slider.value++;
   }
   if(e.which === 37 && slider.value > 1){
     slider.value--;
   }
-  app.gameView.updateTurn(slider.value);
   $('.range-handle').css('left',parseInt(slider.value));
 	$('.range-quantity').css('width',parseInt(slider.value));
 });
