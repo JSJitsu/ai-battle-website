c828cb74403fc82346473185e8e0e692c53b855a
Yahkob <jakobharclerode@gmail.com>
2014-08-05 11:54:14 -0700

    feature(public): Fix 'bouncing'

--- public/js/GameView.js
+++ public/js/GameView.js
@@ -4,19 +4,22 @@ var GameView = Backbone.View.extend({
   initialize: function(){
     this.updateTurn(1);
     //what to operate on
-    var source = $("#turn").html();
+    var turn = $("#turn").html();
     //compiling handlebars
-    this.template = Handlebars.compile(source);
+    this.turnTemplate = Handlebars.compile(turn);
   },
 
   render: function(){
   	this.$el.html('');
-    this.$el.append('<p>' + this.model.get('diamondMessages') + '</p>').fadeIn();
+    this.$el.append('<div class="messages"></div>');
+    $('.messages').append(this.model.get('diamondMessages'));
+    $('.messages').append('<br>' + this.model.get('attackMessages'));
+    $('.messages').append('<br>' + this.model.get('killMessages'));
     // this.el.append(this.model.get('messages'));
     this.context = {round: this.model.get('turn')};
   	var boardView = new BoardView({collection: this.model.get('board')});
-    var html = this.template(this.context);
-    this.$el.append(html);
+    var turnHtml = this.turnTemplate(this.context);
+    this.$el.append(turnHtml);
     this.$el.append(boardView.$el);
   },
   updateTurn: function(turn) {
