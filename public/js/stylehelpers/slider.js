b8ce81981d80568330ef5a88df2063607f146d07
Yahkob <jakobharclerode@gmail.com>
2014-08-04 23:07:04 -0700

    feature(slider): implement basic scrobbling behavior

--- public/js/stylehelpers/slider.js
+++ public/js/stylehelpers/slider.js
@@ -3,20 +3,28 @@ var slider = document.querySelector('.slider');
 //intialize new slider
 var init = new Powerange(slider, { hideRange: false, min: 1, max: 2000, start: 1});
 //listens for slider changes and renders backbone view accordingly
-
+var sliderPos = slider.value;
+var count = 0;
 slider.onchange = function() {
-	app.gameView.updateTurn(slider.value);
+  app.gameView.updateTurn(slider.value);
+  console.log(count, '-count in drag', slider.value + '-slider value in drag') ;
 };
 //listens for keydown events to control scrobbling
 $(document).keydown(function(e){
-  var turn = app.gameView.get('turn');
-  if(e.which === 39 && slider.value < 2000){
-
-    slider.value++;
+  count = app.game.get('turn');
+  sliderPos = slider.value;
+  // var turn = app.gameView.get('turn');
+  if(e.which === 39 && slider.value < 580){
+    console.log(count, '-count in key', slider.value + '-slider value in key');
+    slider.value = parseFloat(slider.value) + 0.29;
+    sliderPos = parseFloat(sliderPos) + .29
+    parseInt(count++);
+    app.gameView.updateTurn(count);
+    app.game.updateTurn(count);    
   }
   if(e.which === 37 && slider.value > 1){
     slider.value--;
   }
-  $('.range-handle').css('left',parseInt(slider.value));
-	$('.range-quantity').css('width',parseInt(slider.value));
+  $('.range-handle').css('left',sliderPos);
+  $('.range-quantity').css('width',parseInt(slider.value));
 });
