aa2b4f6c19f1f7efbe236412280c9136e91efb53
Yahkob <jakobharclerode@gmail.com>
2014-08-05 17:35:59 -0700

    feature(public): add IDs

--- public/js/BoardTileView.js
+++ public/js/BoardTileView.js
@@ -5,13 +5,13 @@ var BoardTileView = Backbone.View.extend({
     if (this.model === undefined) {
       console.log('UNDEFINED!');
     }
-
     this.render();
     this.model.on('change', this.render());
   },
   render: function() {
     var subType = this.model.get('subType');
-    var type = this.model.get('type')
+    var type = this.model.get('type');
+    var teamId = this.model.get('team');
     if (subType !== 'Unoccupied') {
       var assets = {
         Tree: '../img/bush.png',
@@ -23,8 +23,12 @@ var BoardTileView = Backbone.View.extend({
       };
       var html = '<img src="' + assets[subType] + '">';
       if (type === 'Hero') {
-
-        html += '<div class="hero">'+ this.model.get('battleId') + '</div>';
+        var colors = {
+          0: "team-yellow",
+          1: "team-blue"
+        }
+        console.log(colors[this.model.get('team')]);
+        html += '<div class="hero ' + colors[this.model.get('team')] +'">' + this.model.get('battleId') + '</div>';
       }
       this.$el.html(html);
     }
