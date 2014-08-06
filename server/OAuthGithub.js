9710d81661a0c014ab766e2b65cf69d46e4658b6
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-08-06 09:26:53 -0700

    Testing OAuth solution

--- server/OAuthGithub.js
+++ server/OAuthGithub.js
@@ -1,5 +1,7 @@
 var GitHubStrategy = require('passport-github').Strategy;
 var passport = require('passport');
+var express = require('express');
+var session = require('express-session');
 
 
 module.exports = function(app) {
@@ -8,45 +10,69 @@ module.exports = function(app) {
   var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || 'local no id';
   var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || 'local no id';
 
+  var strategy = new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: 'http://localhost:8080/auth/github/callback'
+  });
+  passport.use('github', strategy, function(accessToken, refreshToken, profile, done) {
+    done(null, profile);
+    process.nextTick(function() {
+    });
+  });
 
-// app.configure(function() {
-//   app.use(passport.initialize());
-//   app.use(express.session({ secret: 'keyboard cat' }));
-//   app.use(passport.session());
-// });
+  // passport.use(new GitHubStrategy({
+  //     clientID: GITHUB_CLIENT_ID,
+  //     clientSecret: GITHUB_CLIENT_SECRET,
+  //     callbackURL: "http://www.javascriptbattle.com/"
+  //   },
+  //   function(accessToken, refreshToken, profile, done) {
+  //     console.log('accessToken in passport.use callback: ', accessToken);
+  //     console.log('refreshToken in passport.use callback: ', refreshToken);
+  //     console.log('profile in passport.use callback: ', profile);
+  //     process.nextTick(function () {
+  //       return done(null, profile);
+  //     });
+  //   }
+  // ));
 
-passport.use('github', new GitHubStrategy({
-      clientID: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: "http://www.javascriptbattle.com/"
-  },
-  function(accessToken, refreshToken, profile, done) {
-      // find or create user in database and return user not entire profile to 'done'
-      // User.findOrCreate(..., function(err, user) {
-      // done(err, user);
-      console.log('accessToken in passport.use callback: ', accessToken);
-      console.log('refreshToken in passport.use callback: ', refreshToken);
-      console.log('profile in passport.use callback: ', profile);
-      return done(null, profile);
-      //});
-  }
-));
+  passport.serializeUser(function(user, done) {
+    console.log('user in serializeUser: ', user);
+    done(null, user);
+  });
 
+  passport.deserializeUser(function(obj, done) {
+    console.log('obj in deserializeUser: ', obj);
+    done(null, obj);
+  });
 
-passport.serializeUser(function(user, done) {
-  console.log('user in serializeUser: ', user);
-  done(null, user.id);
-});
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-passport.deserializeUser(function(id, done) {
-  console.log('id in deserializeUser: ', id);
-  // find user by id in db and return user
-  // done(null, user);
-  done(null, id);
-});
+  app.get('/auth/github',
+    passport.authenticate('github'),
+    function(req, res) {
+      //redirected to github for authentication
+      //so code should never get here
+    });
 
-app.get('/auth/github', passport.authenticate('github'));
+  app.get('/success', function(req, res, next) {
+    res.send('Successfully logged in.');
+  });
+   
+  app.get('/error', function(req, res, next) {
+    res.send("Error logging in.");
+  });
 
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { successRedirect: '/', failureRedirect: '/login' }));
+  app.get('/auth/github/callback', 
+    passport.authenticate('github', { 
+      successRedirect: 'http://localhost:8080/success',
+      failureRedirect: 'http://localhost:8080/error'
+    }));
+
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
 };
