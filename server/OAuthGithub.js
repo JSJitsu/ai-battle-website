39b5ce8c5fc854f193a548f799073c3217a162a6
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-08-06 18:20:02 -0700

    feature(OAuthGithub.js): Save users to database when they log in.

--- server/OAuthGithub.js
+++ server/OAuthGithub.js
@@ -8,9 +8,11 @@ var Q = require('q');
 mongoose.connect('mongodb://localhost/javascriptBattle');
 
 var userSchema = mongoose.Schema({
-  id: String,
   githubHandle: String,
-  codeRepo: String
+  codeRepo: {
+    type: String,
+    default: ''
+  }
 });
 
 var User = mongoose.model('User', userSchema);
@@ -20,22 +22,44 @@ module.exports = function(app) {
   app.use(session({ secret: 'thisisasecret'}));
   app.use(passport.initialize());
   app.use(passport.session());
-  app.use(function(req, res, next) {
-    console.log(req.user);
-    next();
-  });
 
-  passport.serializeUser(function(user, done) {
+  //Convert the user object from github into something smaller that
+  //can be stored in a cookie
+  passport.serializeUser(function(githubUser, done) {
     //Check if user exists
+    Q.ninvoke(User, 'find', {
+      githubHandle: githubUser.username
+    }).then(function(user) {
+      //No user found, need to create one
+      if (user.length === 0) {
+        //Create new user object
+        var user = new User({
+          githubHandle: githubUser.username
+        });
 
+        //Save user (return the promise of saving the user)
+        return Q.ninvoke(user, 'save');
 
-    // console.log('user in serializeUser: ', user);
-    done(null, user.id);
+      //This user already exists, return it
+      } else {
+        return user[0];
+      }
+    }).then(function(user) {
+      done(null, user._id);
+    }).catch(function(err) {
+      console.log(err);
+    });
   });
 
-  passport.deserializeUser(function(obj, done) {
-    // console.log('obj in deserializeUser: ', obj);
-    done(null, obj);
+  //Convert the hero id stored in the cookie into the user object
+  //in our database
+  passport.deserializeUser(function(mongoId, done) {
+    //continue with the user with the matching Id
+    Q.ninvoke(User, 'findById', mongoId).then(function(user) {
+      done(null, user);
+    }).catch(function(err) {
+      res.send(err);
+    });
   });
   
   var GITHUB_CLIENT_ID = 'd0d7cc13b38c11adb842';
@@ -56,7 +80,20 @@ module.exports = function(app) {
   var GITHUB_CLIENT_ID = 'd0d7cc13b38c11adb842';
   var GITHUB_CLIENT_SECRET = 'f745c3ff8e2718cf4f0debabd8d0abbcace5b91f';
 
+  //Go here to login
   app.get('/auth/github',passport.authenticate('github'));
+
+  //Go here to log out
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  //This is where github sends us after it finishes authenticating us
+  app.get('/auth/github/callback', passport.authenticate('github', { 
+    successRedirect: '/success',
+    failureRedirect: '/error'
+  }));
 
   app.get('/success', function(req, res, next) {
     res.send('Successfully logged in.');
@@ -64,16 +101,6 @@ module.exports = function(app) {
    
   app.get('/error', function(req, res, next) {
     res.send("Error logging in.");
-  });
-
-  app.get('/auth/github/callback', passport.authenticate('github', { 
-      successRedirect: '/success',
-      failureRedirect: '/error'
-    }));
-
-  app.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
   });
 
   var ensureAuthenticated = function(req, res, next) {
@@ -87,6 +114,5 @@ module.exports = function(app) {
   app.get('/restricted', ensureAuthenticated, function(req, res) {
     res.send('you are IN!');
   });
-
 
 };
