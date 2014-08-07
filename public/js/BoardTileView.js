2ba78616dbae19582c7d504316de931987d04129
Yahkob <jakobharclerode@gmail.com>
2014-08-06 19:46:47 -0700

    feature('public'): edit tile highlight

--- public/js/BoardTileView.js
+++ public/js/BoardTileView.js
@@ -30,7 +30,7 @@ var BoardTileView = Backbone.View.extend({
         var gameTurn = this.model.get('gameTurn');
         var lastActiveTurn = this.model.get('lastActiveTurn')
         if(lastActiveTurn === (gameTurn - 1) && gameTurn !== 1){
-          this.$el.addClass('current-turn');
+          console.log(this.$el.innerHTML)
         }
         html = '<img src="' + assets[subType] + '" class="H' + this.model.get('battleId') +'">';
         
