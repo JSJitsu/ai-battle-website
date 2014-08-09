c2319bb4e95ddc643c89c82de85f75649469648d
forrestbthomas <forrestbthomas@gmail.com>
2014-08-08 19:38:58 -0700

    feature(githubRequest): Add mongolab integration with github request

--- azureScripts/githubRequest.js
+++ azureScripts/githubRequest.js
@@ -1,26 +1,50 @@
 var request = require('request');
+var MongoClient = require('mongodb').MongoClient;
+var mongoConnectionURL = process.env.CUSTOMCONNSTR_MONGO_URI;
+var Q = require('q');
 var fs = require('fs');
-var options = {
-  url: 'https://api.github.com/repos/forrestbthomas/hero-starter/contents/hero.js',
-  headers: {
-    'User-Agent': 'hero-starter'
-  }
+
+var openGameDatabase = function() {
+  return Q.ninvoke(MongoClient, 'connect', mongoConnectionURL).then(function(db) {
+    console.log('open!');
+    return {
+      collection: db.collection('users')
+    };
+  });
 };
 
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    var buffer = new Buffer(info.content, 'base64');
-    var usersCode = buffer.toString('utf8');
-    console.log(usersCode);
-    var regEx = usersCode.match(/\bdocker\b/gi);
-    if (regEx){
-      console.log("Possible Malicious Code.");
-    }
-    fs.writeFile('/Users/forrestbthomas/Documents/hero.js', usersCode, function(err){
-      console.log(err);
+var usersCodeRequest = function () {
+  var openDatabasePromise = openGameDatabase();
+  openDatabasePromise.then(function(data){
+    data.collection.find().toArray(function(err, results){
+      for (var i = 0; i < results.length; i++){
+        console.log('LOOPING');
+        var currentUser = results[i];
+        var options = {
+          url: 'https://api.github.com/repos/' + currentUser.githubHandle + 
+          '/' + currentUser.codeRepo + '/contents/hero.js',
+          headers: {
+            'User-Agent': 'hero-starter'
+          }
+        };
+        var callback = function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body);
+            var buffer = new Buffer(info.content, 'base64');
+            var usersCode = buffer.toString('utf8');
+            console.log(usersCode);
+            var regEx = usersCode.match(/\bdocker\b/gi);
+            if (regEx){
+              console.log("Possible Malicious Code.");
+            }
+            fs.writeFile('./UsersCode/' + currentUser.githubHandle + '_hero.js', usersCode, function(err){
+              console.log(err);
+            });
+          } 
+        };
+        request(options, callback);
+      }
     });
-  }
-}
-
-request(options, callback);
+  })
+};
+usersCodeRequest(); 
