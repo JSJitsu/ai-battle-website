8c794ac402866787cd93ef79b19c965523a83707
jamesyothers <james.yothers@gmail.com>
2014-08-11 10:30:23 -0700

    change submit to save in loggedIn template

--- public/js/UserView.js
+++ public/js/UserView.js
@@ -13,6 +13,7 @@ var UserView = Backbone.View.extend({
     this.render();
     // fetch will get object at model's url
     // can use 'parse' as middleware for object
+    // jQuery promise
     $.when(this.model.fetch()).then(function() {
       this.render();
     }.bind(this));
