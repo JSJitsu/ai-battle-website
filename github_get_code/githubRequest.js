2b753c39973aff2968cc87b3a95404e14a6dcae5
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-08-11 15:48:44 -0700

    Restructure repository

---
+++ github_get_code/githubRequest.js
@@ -0,0 +1,80 @@
+var request = require('../node_modules/request');
+var MongoClient = require('../node_modules/mongodb').MongoClient;
+var Q = require('../node_modules/q');
+var fs = require('fs');
+var secrets = require('../secrets.js');
+var mongoConnectionURL = secrets.mongoKey;
+
+//Returns a promise that resolves when the database opens
+var openGameDatabase = function() {
+  return Q.ninvoke(MongoClient, 'connect', mongoConnectionURL).then(function(db) {
+    console.log('open!');
+    return {
+      db: db
+      users: db.collection('users'),
+    };
+  });
+};
+
+var usersCodeRequest = function () {
+
+  //Opens connection to mongo database
+  var openDatabasePromise = openGameDatabase();
+
+  openDatabasePromise.then(function(mongoDataObject) {
+    var userCollection = mongoDataObject.users;
+
+    //Gets all users in the database
+    userCollection.find().toArray(function(err, users) {
+      //Loops through all users
+      results.forEach(function(currentUser, ind) {
+
+        var options = {
+          //Saves the URL at which the hero code can be found
+          url: 'https:/' + secrets.apiUser + ':' + secrets.apiPass + '@api.github.com/repos/' + currentUser.githubHandle + 
+          '/' + currentUser.codeRepo + '/contents/hero.js',
+
+          //Needed by the github API
+          headers: {
+            'User-Agent': secrets.apiUser
+          }
+        };
+
+        //Sends the request for each user's hero.js file to the github API
+        request(options, function (error, response, body) {
+          if (error){
+            console.log(error)
+            return;
+          };
+
+          //If everything is ok, save the file
+          if (response.statusCode == 200) {
+            //Get response as JSON
+            var info = JSON.parse(body);
+
+            //Set up buffer to write file
+            var buffer = new Buffer(info.content, 'base64');
+
+            //Convert buffer to long string
+            var usersCode = buffer.toString('utf8');
+
+            //Check for "docker" anywhere in the file
+            var regEx = usersCode.match(/\bdocker\b/gi);
+            if (regEx){
+              console.log("Possible Malicious Code.");
+              return;
+            }
+
+            //Write the file to a predefined folder and file name
+            fs.writeFile(secrets.userCodeFolder + currentUser.githubHandle + '_hero.js', usersCode, function(err) {
+              console.log(err);
+            });
+          }
+        });
+      });
+    });
+  });
+};
+
+//Kick off the whole process
+usersCodeRequest();
