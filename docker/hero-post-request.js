cd95e013758c7fa453246241e6cd9f8a0ab71c94
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-08-11 20:43:35 -0700

    Start modifying hero post request

--- docker/hero-post-request.js
+++ docker/hero-post-request.js
@@ -1,14 +1,16 @@
 var request = require('request');
 var fs = require('fs');
+var Q = require('q');
 
-var postFileToServer = function(port, filePath){
+var postFileToServer = function(port, filePath) {
   var url = 'http://0.0.0.0:' + port;
-	var r = request.post(url, function(err, res, body) {
-    console.log(res.body);
-	});
 
-	var form = r.form();
-	form.append('my_file', fs.createReadStream(filePath));
+  var r = request.post(url, function(err, res, body) {
+    console.log(res.body);
+  });
+
+  var form = r.form();
+  form.append('my_file', fs.createReadStream(filePath));
 };
 
 module.exports = postFileToServer;
