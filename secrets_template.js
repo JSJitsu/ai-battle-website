62f28e8ca2749506d0714b6c9fe0acc95ff28eca
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-08-14 15:36:58 -0700

    refactor(): Clean up a few files

---
+++ secrets_template.js
@@ -0,0 +1,9 @@
+var secrets = module.exports = {};
+
+secrets.mongoKey = 'mongo connection url here';
+
+secrets.apiUser = 'a github account handle here';
+
+secrets.apiPass = 'password to the github account here';
+
+secrets.rootDirectory = 'the local address of the root of this repo';
