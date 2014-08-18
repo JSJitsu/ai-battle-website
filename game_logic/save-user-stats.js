0e0716f0d83188611acc58ba05744c5dbfdb8fbe
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-08-18 16:40:03 -0700

    feature(save-user-stats.js): Add most recent stats to user updating.

--- game_logic/save-user-stats.js
+++ game_logic/save-user-stats.js
@@ -10,27 +10,38 @@ module.exports = function(mongoData, hero) {
   //Grab the user from the database
   return Q.ninvoke(userCollection, 'findOne', { githubHandle: hero.name }).then(function(user) {
 
-    //Update the user stats
+    //Update the user's lifetime and most recent stats
     user.lifetimeStats.kills += hero.heroesKilled.length;
+    user.mostRecentStats.kills = hero.heroesKilled.length;
 
     if (hero.dead) {
       user.lifetimeStats.deaths++;
+      user.mostRecentStats.survived = false;
+    } else {
+      user.mostRecentStats.survived = true;
     }
 
     user.lifetimeStats.damageDealt += hero.damageDone;
+    user.mostRecentStats.damageDealt = hero.damageDone;
 
     user.lifetimeStats.minesCaptured += hero.minesCaptured;
+    user.mostRecentStats.minesCaptured = hero.minesCaptured;
 
     user.lifetimeStats.diamondsEarned += hero.diamondsEarned;
+    user.mostRecentStats.diamondsEarned = hero.diamondsEarned;
 
     user.lifetimeStats.healthRecovered += hero.healthRecovered;
+    user.mostRecentStats.healthRecovered = hero.healthRecovered;
 
     user.lifetimeStats.gravesRobbed += hero.gravesRobbed;
+    user.mostRecentStats.gravesRobbed = hero.gravesRobbed;
 
     if (hero.won) {
       user.lifetimeStats.wins++;
+      user.mostRecentStats.gameResult = 'Win';
     } else {
       user.lifetimeStats.losses++;
+      user.mostRecentStats.gameResult = 'Loss';
     }
 
     return user;
@@ -41,7 +52,7 @@ module.exports = function(mongoData, hero) {
     //Save the user stats
     return Q.npost(mongoData.userCollection, 'update', [
       {
-        '_id':user._id
+        '_id': user._id
       }, user, {
         upsert:true
       }
