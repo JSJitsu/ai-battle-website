2f35ad273cbe0b9c9989cf9503e56939a06aa53d
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-08-19 11:52:37 -0700

    fix(server): Add seven hour time offset back in when server is in production

--- server.js
+++ server.js
@@ -92,6 +92,9 @@ var getDateString = function(dayOffset) {
   if (dayOffset === undefined) {
     dayOffset = 0;
   }
+  if (productionMode === 'production') {
+    dayOffset -= 7/24;
+  }
   var jsDate = new Date((new Date()).getTime() + dayOffset*24*60*60*1000);
   var result = (jsDate.getMonth() + 1).toString();
   result += '/' + jsDate.getDate();
