c73fad9a3fb842c56e6a386054dcc0ed8a96d1fc
jamesyothers <james.yothers@gmail.com>
2014-08-19 17:01:12 -0700

    display two decimal places in average user stats

--- public/js/User.js
+++ public/js/User.js
@@ -9,16 +9,15 @@ var User = Backbone.Model.extend({
 
   average: function() {
     var gamesPlayed = this.get('lifetimeStats').wins + this.get('lifetimeStats').losses;
-
     var aveStats = {
       gamesPlayed: gamesPlayed,
-      kills: (this.get('lifetimeStats').kills / gamesPlayed) || this.get('lifetimeStats').kills,
-      deaths: (this.get('lifetimeStats').deaths / gamesPlayed) || this.get('lifetimeStats').deaths,
-      damageDealt: (this.get('lifetimeStats').damageDealt / gamesPlayed) || this.get('lifetimeStats').damageDealt,
-      minesCaptured: (this.get('lifetimeStats').minesCaptured / gamesPlayed) || this.get('lifetimeStats').minesCaptured,
-      diamondsEarned: (this.get('lifetimeStats').diamondsEarned / gamesPlayed) || this.get('lifetimeStats').diamondsEarned,
-      healthRecovered: (this.get('lifetimeStats').healthRecovered / gamesPlayed) || this.get('lifetimeStats').healthRecovered,
-      gravesRobbed: (this.get('lifetimeStats').gravesRobbed / gamesPlayed) || this.get('lifetimeStats').gravesRobbed
+      kills: (Number(parseFloat(this.get('lifetimeStats').kills / gamesPlayed).toFixed(2)) || this.get('lifetimeStats').kills) + ' per game',
+      deaths: (Number(parseFloat(this.get('lifetimeStats').deaths / gamesPlayed).toFixed(2)) || this.get('lifetimeStats').deaths) + ' per game',
+      damageDealt: (Number(parseFloat(this.get('lifetimeStats').damageDealt / gamesPlayed).toFixed(2)) || this.get('lifetimeStats').damageDealt) + ' per game',
+      minesCaptured: (Number(parseFloat(this.get('lifetimeStats').minesCaptured / gamesPlayed).toFixed(2)) || this.get('lifetimeStats').minesCaptured) + ' per game',
+      diamondsEarned: (Number(parseFloat(this.get('lifetimeStats').diamondsEarned / gamesPlayed).toFixed(2)) || this.get('lifetimeStats').diamondsEarned) + ' per game',
+      healthRecovered: (Number(parseFloat(this.get('lifetimeStats').healthRecovered / gamesPlayed).toFixed(2)) || this.get('lifetimeStats').healthRecovered) + ' per game',
+      gravesRobbed: (Number(parseFloat(this.get('lifetimeStats').gravesRobbed / gamesPlayed).toFixed(2)) || this.get('lifetimeStats').gravesRobbed) + ' per game'
     }; 
     return aveStats;
   }
