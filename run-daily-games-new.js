0de8d671e6ac8b28f80f0f8b1996f15b5abab293
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-08-27 22:19:25 -0700

    Start new daily runner to use the new system

--- run-daily-games-new.js
+++ run-daily-games-new.js
@@ -27,63 +27,21 @@ var usersCodeRequest = function() {
     //Get all user containers ready for game
     }).then(function(users) {
 
-      //Get the users for the first game
+      console.log('Preparing users...');
 
+      //Get the users for the first game
+      return prepareUserContainers(users);
       
 
     //All user containers are 100% ready--run this game
-    }).then(function(users) {
+    }).then(function() {
 
-    })
+      console.log('All users prepared!');
 
-    //Get array of all users that have assigned ports
-    userCollection.find({ 
-      port: {
-        $gt: 12499
-      }
-    }).toArray(function(err, users) {
-      if (err) {
-        console.log('Error finding users!');
-        console.log(err);
-        db.close();
-        return;
-      }
-
-      //Get the docker containers and hero brains ready to roll
-      Q.all(users.map(function(user) {
-        var pathToHeroBrain = secrets.rootDirectory + '/user_code/hero/' + user.githubHandle + '_hero.js';
-        var pathToHelperFile = secrets.rootDirectory + '/user_code/helpers/' + user.githubHandle + '_helpers.js';
-        
-        //send the hero brain to the server in the container
-        return communicateWithContainers.postFile(user.port, pathToHeroBrain, 'hero').then(function() {
-          //send the helper file to the server in the container
-          return communicateWithContainers.postFile(user.port, pathToHelperFile, 'helper');
-        });
-
-      //Start the game
-      })).then(function(value) {
-        console.log(value);
-        console.log('All hero brain containers are ready...starting the game!');
-        
-        //Loop through the turns, send gameData, get responses, etc
-        return createAndSaveAllGames(users, mongoData);
-
-      }).then(function() {
-        console.log('Game has finished successfully!');
-        console.log('Closing database connection...');
-        db.close();
-
-      }).catch(function(err) {
-        console.log('ERROR!');
-        console.log(err);
-        console.log('Closing db connection...');
-        db.close();
-      });
+    }).catch(function(err) {
+      console.log('ERROR!');
+      console.log(err);
     });
-  }).catch(function(err) {
-    console.log('Error opening database!');
-    console.log(err);
-  });
 };
 
 usersCodeRequest();
