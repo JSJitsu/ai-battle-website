9cc14386649ee638603731f305a24242b29c3d93
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-10-12 11:53:30 -0400

    fix(open-mongo-database): Add connection options
    Added keepAlive and connectTimeoutMS options when opening mongo databases.

--- helpers/open-mongo-database.js
+++ helpers/open-mongo-database.js
@@ -3,11 +3,26 @@ var Q = require('q');
 var secrets = require('../secrets.js');
 var defaultConnectionURL = secrets.mongoKey;
 
+var mongoOptions = {
+  server: {
+    socketOptions: {
+      keepAlive: 1,
+      connectTimeoutMS: 30000
+    }
+  },
+  replset: {
+    socketOptions: {
+      keepAlive: 1,
+      connectTimeoutMS: 30000
+    }
+  }
+};
+
 //Returns a promise that opens the mongo database
 var openDatabase = function(mongoConnectionURL) {
   mongoConnectionURL = mongoConnectionURL || defaultConnectionURL;
 
-  return Q.ninvoke(MongoClient, 'connect', mongoConnectionURL).then(function(db) {
+  return Q.ninvoke(MongoClient, 'connect', mongoConnectionURL, mongoOptions).then(function(db) {
     console.log('Mongo database is connected!');
     return {
       db: db,
