0a852444aad09e4a6dc7c2636bec7377caa43bc4
Greg <gjtrowbridge@gmail.com>
2014-10-19 01:18:52 -0400

    Make start-stop-containers indenting consistent

--- docker/container_interaction/start-stop-containers.js
+++ docker/container_interaction/start-stop-containers.js
@@ -10,6 +10,36 @@ startStopContainersObj.spinUpContainer = function(port) {
   return spawn(secrets.rootDirectory + 
       '/docker/container_interaction/start-hero-brain-container.sh', [port])
 
+  .progress(function(childProcess) {
+    console.log('[spawn] childProcess.pid: ', childProcess.pid);
+    childProcess.stdout.on('data', function(data) {
+        console.log('  [spawn] stdout: ', data.toString());
+    });
+    childProcess.stderr.on('data', function(data) {
+        console.log('  [spawn] stderr: ', data.toString()); 
+    });
+  });
+    
+};
+
+//Return a promise that resolves after stopping all containers,
+//then removing all containers
+startStopContainersObj.shutDownAllContainers = function() {
+  return exec('sudo docker stop $(sudo docker ps -q -a)')
+
+  .progress(function(childProcess) {
+    console.log('[spawn] childProcess.pid: ', childProcess.pid);
+    childProcess.stdout.on('data', function(data) {
+        console.log('  [spawn] stdout: ', data.toString());
+    });
+    childProcess.stderr.on('data', function(data) {
+        console.log('  [spawn] stderr: ', data.toString()); 
+    });
+  })
+
+  .then(function() {
+    return exec('sudo docker rm -f $(sudo docker ps -a -q)')
+
     .progress(function(childProcess) {
       console.log('[spawn] childProcess.pid: ', childProcess.pid);
       childProcess.stdout.on('data', function(data) {
@@ -20,32 +50,7 @@ startStopContainersObj.spinUpContainer = function(port) {
       });
     });
     
-};
-
-//Return a promise that resolves after stopping all containers,
-//then removing all containers
-startStopContainersObj.shutDownAllContainers = function() {
-  return exec('sudo docker stop $(sudo docker ps -q -a)')
-    .progress(function(childProcess) {
-      console.log('[spawn] childProcess.pid: ', childProcess.pid);
-      childProcess.stdout.on('data', function(data) {
-          console.log('  [spawn] stdout: ', data.toString());
-      });
-      childProcess.stderr.on('data', function(data) {
-          console.log('  [spawn] stderr: ', data.toString()); 
-      });
-    }).then(function() {
-      return exec('sudo docker rm -f $(sudo docker ps -a -q)')
-        .progress(function(childProcess) {
-          console.log('[spawn] childProcess.pid: ', childProcess.pid);
-          childProcess.stdout.on('data', function(data) {
-              console.log('  [spawn] stdout: ', data.toString());
-          });
-          childProcess.stderr.on('data', function(data) {
-              console.log('  [spawn] stderr: ', data.toString()); 
-          });
-        });
-    });
+  });
 };
 
 module.exports = startStopContainersObj;
