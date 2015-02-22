90fc7af3540f5f78338fd2820d00575030af0e84
Jake Harclerode <Jakobharclerode@gmail.com>
2015-02-21 22:52:56 -0800

    fix row rendering issues

--- public/js/board/BoardView.js
+++ public/js/board/BoardView.js
@@ -2,37 +2,23 @@ var BoardView = Backbone.View.extend({
   tagName: 'section',
   className: 'battle-map',
   initialize: function() {
-    this.render();
+    this.render()
   },
   render: function() {
     this.$el.html('');
     this.createBoardView();
   },
   createBoardView: function() {
-  	var boardLength = this.collection.lengthOfSide;
-    var $tr = $('<div class="tile-row">');
-    // var models = this.collection.models;
-    // for (var i = 0; i < this.collection.models.length; i++) {
-    //   if (!i || (i % boardLength) === 0) {
-    //     $tr = $('<div class="tile-row">'); 
-    //   }
-    //   var tileView = new BoardTileView({
-    //     model: models[i]          
-    //   });
-    //   $tr.append(tileView.$el);
-    //   if(i && (i % boardLength) === 0){
-    //     this.$el.append($tr);
-    //   }
-    // }       
+    var boardLength = this.collection.lengthOfSide;
     for(var i = 0; i < boardLength; i++){
       var $tr = $('<div class="tile-row">');
-    	for(var j = 0; j < boardLength; j++){
+      for(var j = 0; j < boardLength; j++){
         var tileView = new BoardTileView({
-    			model: this.collection.at(i * boardLength + j)          
-    		});
-    	  $tr.append(tileView.$el);
-    	}
-    	this.$el.append($tr);
+          model: this.collection.at(i * boardLength + j)
+        });
+        $tr.append(tileView.$el);
+      }
+      this.$el.append($tr);
     }
   }
 });
