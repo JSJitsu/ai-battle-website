df6dc2e2ca203be82f2c12fd42c1f5c30e0fb24d
John Krull <astrom.flux@gmail.com>
2016-05-14 16:18:22 +0000

    Fix and simplify leaderboard

--- server.js
+++ server.js
@@ -92,7 +92,34 @@ router.get('/game(/:id)?', function(req, res) {
 
 // Returns the leaderboard data for the specified time period and stat
 router.get('/leaderboard/:timePeriod/:stat', function(req, res) {
-    res.status(200).send();
+
+    var stat = req.params.stat;
+    var allowedStats = {
+        games_won: 'games_won',
+        kills: 'kills',
+        graves_taken: 'graves_taken',
+        diamonds_earned: 'diamonds_earned',
+        health_given: 'health_given'
+    };
+
+    var chosenStat = allowedStats[stat];
+
+    if (!chosenStat) {
+        res.status(404).send();
+        return;
+    }
+
+    return Q.ninvoke(db, 'query', `SELECT * FROM player_lifetime_stats ORDER BY ${chosenStat} DESC LIMIT 20`)
+        .then(function (stats) {
+            if (stats) {
+                res.status(200).send({
+                    stats: stats
+                });
+            } else {
+                res.status(404).send();
+            }
+        }
+    );
 });
 
 // Set root route for app's data
