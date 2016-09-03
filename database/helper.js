a13954d73f91f577fa5c6d5c964e86072199a6e1
John Krull <astrom.flux@gmail.com>
2016-09-03 23:47:47 +0000

    Clean up and simplify user authentication

--- database/helper.js
+++ database/helper.js
@@ -64,8 +64,53 @@ class Helper {
         return sql;
     }
 
+    performQuery (query) {
+        return Q.ninvoke(this.database, 'query', query).catch(this.errorHandler);
+    }
+
+    performUpdate (query, record) {
+        return Q.ninvoke(this.database, 'query', query, record).catch(this.errorHandler);
+    }
+
+    errorHandler (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+
+    getFirstResult (results) {
+        return results[0];
+    }
+
     getAllPlayerLifetimeStats () {
-        return Q.ninvoke(this.database, 'query', "SELECT * FROM player_lifetime_stats");
+        return this.performQuery("SELECT * FROM player_lifetime_stats");
+    }
+
+    getPlayerLifetimeStats (username) {
+        return this.performQuery(`SELECT * FROM player_lifetime_stats WHERE github_login = '${username}'`);
+    }
+
+    getLatestGameResultByUsername (username) {
+        return this.performQuery(`SELECT * FROM game_results WHERE '${username}' = ANY(players) ORDER BY game_id DESC LIMIT 1`);
+    }
+
+    getAllGameResultsByUsername (username) {
+        return this.performQuery(`SELECT * FROM game_results WHERE '${username}' = ANY(players) ORDER BY game_id DESC`);
+    }
+
+    getPlayer (username) {
+        return this.performQuery(`SELECT * FROM player WHERE github_login = '${username}'`);
+    }
+
+    updatePlayer (record) {
+        let updateSql = this.updateSql('player', record, `github_login = '${record.github_login}'`);
+
+        return this.performUpdate(updateSql, record);
+    }
+
+    insertPlayer (record) {
+        let insertSql = this.insertSql('player', record, 'github_login');
+
+        return this.performUpdate(insertSql, record);
     }
 
 }
