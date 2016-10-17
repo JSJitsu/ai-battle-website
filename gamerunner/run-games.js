1312e056845b4b58fa41cbd2d0ea4a9999e77947
John Krull <astrom.flux@gmail.com>
2016-10-17 19:50:06 +0000

    Get user code before running games

--- gamerunner/run-games.js
+++ gamerunner/run-games.js
@@ -1,4 +1,17 @@
-var LiveGameRunner = require('./game_logic/LiveGameRunner.js'),
-    gameRunner = new LiveGameRunner();
+let LiveGameRunner = require('./game_logic/LiveGameRunner.js');
+let gameRunner = new LiveGameRunner();
+
+let cliOpts = require('minimist')(process.argv.slice(2));
+
+if (!cliOpts['skip-fetch']) {
+    let path = require('path');
+    let childProcess = require('child_process');
+
+    let scriptPath = path.resolve(__dirname, 'user_code', 'get-user-code-from-github.js');
+
+    childProcess.execSync(`node ${scriptPath}`, {
+        stdio: 'inherit'
+    });
+}
 
 gameRunner.run();
