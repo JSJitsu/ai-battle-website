225de34a1801522b476687bb66b62923aac46bc1
Ethan HO <ethan.hobl@gmail.com>
2016-12-13 22:18:36 +1100

    updated server.js's cli input parser to commander.js

--- server.js
+++ server.js
@@ -5,10 +5,29 @@ const fs = require('fs');
 const Q = require('q');
 
 const OAuthGithub = require('./server/OAuthGithub');
-const argv = require('minimist')(process.argv.slice(2));
 const config = require('./config');
 const db = require('./database/connect.js');
 const dbHelper = new (require('./database/helper.js'))(db);
+const argv = require('commander');
+argv.description('Usage: ' + process.argv[0] + ' ' + process.argv[1] + ' [options]')
+    .option('--non-github',                    'Do not connect to the GitHub application, **OR**')
+    .option('--github-client-id [string]',     '[Compulsory] - GitHub Application Client ID')
+    .option('--github-client-secret [string]', '[Compulsory] - GitHub Application Client Secret')
+    .option('--github-callback-url [string]',  '[Optional] - Specify the callback used for user auth.')
+    .parse(process.argv);
+if (process.argv.length<3) argv.help();
+
+// cli input validation
+// commander does not support OR condition yet
+if ( typeof(argv.nonGithub)==='undefined' ) {
+    if ( typeof(argv.githubClientId)=='undefined' ||
+        typeof(argv.githubClientSecret)=='undefined' ) {
+        console.log();
+        console.log("  Both github client id and secret is needed!!!");
+        argv.help();
+        process.exit(1);
+    }
+}
 
 // Test the database connection
 db.connect(function (err) {
@@ -20,41 +39,26 @@ db.connect(function (err) {
         db.end();
     }
 
+    console.log("Here : ");
+    console.log("argv.nonGithub          : " + argv.nonGithub);
+    console.log("argv.githubClientId     : " + argv.githubClientId);
+    console.log("argv.githubClientSecret : " + argv.githubClientSecret);
+    console.log("argv.githubCallbackUrl  : " + argv.githubCallbackUrl);
+    process.exit(1);
+
     startServer();
 });
 
 function startServer () {
 
     var options = {
-        useGithubApp: (argv.github === undefined),
+        useGithubApp: ( typeof(argv.nonGithub) === 'undefined' ),
         github: {
-            clientId: argv['github-client-id'],
-            clientSecret: argv['github-client-secret'],
-            callbackUrl: argv['github-callback-url']
+            clientId: argv.githubClientId,
+            clientSecret: argv.githubClientSecret,
+            callbackUrl: argv.githubCallbackUrl
         }
     };
-
-    if (options.useGithubApp && (!options.github.clientId || !options.github.clientSecret)) {
-        var usage = [
-            'Usage: ' + process.argv[0] + ' ' + process.argv[1] + ' [parameters]',
-            '',
-            'Required Parameters:',
-            '',
-            '  --github-client-id      GitHub Application Client ID',
-            '  --github-client-secret  GitHub Application Client Secret',
-            '        OR',
-            '  --no-github             Do not connect to the GitHub application.',
-            '',
-            'Optional Parameters:',
-            '',
-            '  --github-callback-url   Specify the callback used for user auth.',
-            ''
-        ].join('\n');
-
-        console.log(usage);
-
-        process.exit(0);
-    }
 
     const app = express();
 
