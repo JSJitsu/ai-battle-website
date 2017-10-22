e5b61f2ed85ccc5933c8f233588731ebf02bbfc9
Julio Cesar Fausto <jcfausto@gmail.com>
2017-10-22 18:17:58 +0200

    Added Knex to manage Database Schema and Migrations.

--- database/reinitialize.js
+++ database/reinitialize.js
@@ -1,4 +1,39 @@
-require('./destroy.js');
-require('./initialize.js');
+if (process.argv[2] !== '--proceed') {
+    let message = [
+        'This script will drop all data in the database!',
+        'To proceed, run this script again with the "--proceed" flag.'
+    ].join('\n');
 
-process.exit();
+    console.log(message);
+    process.exit(1);
+}
+
+const db = require('./knex');
+
+// Still need to find a way to do the rollback using Knex, that still doesn't
+// suport a reset method or similar.
+let sql = `
+    DROP TABLE IF EXISTS player;
+    DROP TABLE IF EXISTS game_results;
+    DROP TABLE IF EXISTS game;
+    DROP TYPE IF EXISTS actor_action CASCADE;
+    DROP TABLE IF EXISTS game_events;
+    DROP TABLE IF EXISTS player_lifetime_stats;
+    DROP TABLE IF EXISTS knex_migrations;
+    DROP TABLE IF EXISTS knex_migrations_lock;
+`;
+
+db.raw(sql).
+then( () => {
+    console.log('All database tables destroyed.');
+})
+.then( () => {
+    return db.migrate.latest().then( () => {
+        console.log('Database reset completed.');
+        process.exit();
+    });
+})
+.catch(err => {
+    console.error('Error destroying all database tables');
+    console.error(err.message);
+});
