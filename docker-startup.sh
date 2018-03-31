5883f798e52802d6300fddd7e05dfa243f3ed3dc
John Krull <john.a.krull@gmail.com>
2018-03-31 17:02:36 -0500

    Adjust startup helper to use knex

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 service postgresql start
-node database/initialize.js
+node ./node_modules/.bin/knex migrate:latest --knexfile=./database/config/knexfile.js
 su - postgres -c 'cd /app; psql jsfightclub < ./node_modules/connect-pg-simple/table.sql'
 node gamerunner/run-test-game.js
 npm run build
