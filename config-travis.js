e62402188758b59a6de76800bdf62528e2b81b24
John Krull <john.a.krull@gmail.com>
2018-03-31 23:51:19 -0500

    Add postgres to travis build

---
+++ config-travis.js
@@ -0,0 +1,20 @@
+/**
+ * This is used for the travis-ci build process.
+ */
+module.exports = {
+    application: {
+        rootDirectory: __dirname,
+        port: 8080
+    },
+    database: {
+        user: 'postgres',
+        password: '',
+        address: '127.0.0.1',
+        name: 'jsfightclub'
+    },
+    github: {
+        appName: '',
+        appKey: '',
+        appSecret: ''
+    }
+};
